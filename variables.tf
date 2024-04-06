fef3a05c33b9efcbbcc3ac5b320131cc9cc14e39
Gautham Chandra <gautham.chandra@biltcard.com>
2024-04-06 13:58:12 -0400

    feat: Add Terraform GCP configs modified for Org level runners
    The original configs for all this were taken from the
    `terraform-google-github-actions-runners` that can be found here:
    https://github.com/terraform-google-modules/terraform-google-github-actions-runners
    
    However, one core change had to be made to adapt it to our use case:
    Changing it from repositry level runners to organization level runners
    so they can be used by everyone. This is why we have a `modules/`
    directory that references it.

---
+++ variables.tf
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "project_id" {
+  type        = string
+  description = "The project id to deploy Github Runner Managed Instance Group (MIG)"
+}
+
+variable "image" {
+  type        = string
+  description = "The github runner image"
+}
+
+variable "org_url" {
+  type        = string
+  description = "Organization URL for the Github Action"
+}
+
+variable "org_name" {
+  type        = string
+  description = "Name of the Organization for which the Github Action Runner will be registered to"
+}
+
+variable "gh_token" {
+  type        = string
+  description = "Github token that is used for generating Self Hosted Runner Token"
+}
